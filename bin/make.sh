db2cc689544526b245a13549bd67187857f9e07f
delta54 <support@e154.ru>
2017-01-26 01:32:17 +0700

    travis

--- bin/make.sh
+++ bin/make.sh
@@ -114,7 +114,9 @@ __build() {
     cp ${ROOT}/contributors.txt ${TMP_DIR}
     sed 's/dev\/app.conf/prod\/app.conf/' ${ROOT}/conf/app.conf > ${TMP_DIR}/conf/app.conf
     cd ${TMP_DIR}
-    mysqldump -u travis smarthome > ${TMP_DIR}/dump.sql
+    echo "create damp"
+#    mysqldump -u travis smarthome > ${TMP_DIR}/dump.sql
+    echo "tar: ${ARCHIVE} copy to ${HOME}"
     tar zxvf ${ARCHIVE} ${HOME}/
 }
 
