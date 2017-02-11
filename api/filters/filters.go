5732c64c907df8024e9cf25fc6ebff4fa503f02b
delta54 <support@e154.ru>
2017-02-12 01:03:20 +0700

    Authorization update

--- api/filters/filters.go
+++ api/filters/filters.go
@@ -15,7 +15,7 @@ func RegisterFilters() {
 		AllowAllOrigins: true,
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS", "UPDATE"},
 		AllowCredentials: true,
-		AllowHeaders:     []string{"Origin", "Authorization", "Access-Control-Allow-Origin", "token"},
+		AllowHeaders:     []string{"Content-Type", "token"},
 	}))
 
 	// register rbac access filter
