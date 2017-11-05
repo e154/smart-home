31b79fcf3d96b35b087b5c40398a5a24bf6459c6
praetorian <filippov.a@e154.ru>
2017-11-05 18:11:32 +0700

    add constrain to workflow_scenario_scripts

--- database/migrations/20170319_155813_add_workflow_scenario_scripts.sql
+++ database/migrations/20170319_155813_add_workflow_scenario_scripts.sql
@@ -8,8 +8,10 @@ CONSTRAINT unique_id UNIQUE( id ) )
 ENGINE = InnoDB;
 
 ALTER TABLE `workflow_scenario_scripts` ADD CONSTRAINT `lnk_scripts_workflow_scenario_scripts` FOREIGN KEY ( `script_id` ) REFERENCES `scripts`( `id` ) ON DELETE Restrict ON UPDATE Cascade;
+ALTER TABLE `workflow_scenario_scripts` ADD CONSTRAINT `lnk_workflow_scenarios_workflow_scenario_scripts` FOREIGN KEY (`workflow_scenario_id`) REFERENCES `workflow_scenarios` (`id`)  ON UPDATE CASCADE;
 
 -- +migrate Down
 ALTER TABLE `workflow_scenario_scripts` DROP FOREIGN KEY `lnk_scripts_workflow_scenario_scripts`;
+ALTER TABLE `workflow_scenario_scripts` DROP FOREIGN KEY `lnk_workflow_scenarios_workflow_scenario_scripts`;
 DROP TABLE IF EXISTS `workflow_scenario_scripts` CASCADE;
 
