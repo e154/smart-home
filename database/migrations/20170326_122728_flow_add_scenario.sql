9fa8e82ac752713f689399e4ba7c794cad7e0336
praetorian <filippov.a@e154.ru>
2017-11-05 20:48:31 +0700

    add constrain to flow

--- database/migrations/20170326_122728_flow_add_scenario.sql
+++ database/migrations/20170326_122728_flow_add_scenario.sql
@@ -1,5 +1,7 @@
 -- +migrate Up
 ALTER TABLE `flows` ADD COLUMN `workflow_scenario_id` Int( 22 ) NULL;
+ALTER TABLE `flows` ADD CONSTRAINT `lnk_flows_workflow_scenarios` FOREIGN KEY (`workflow_scenario_id`) REFERENCES `workflow_scenarios` (`id`);
 
 -- +migrate Down
+ALTER TABLE `flows` DROP FOREIGN KEY `lnk_flows_workflow_scenarios`;
 ALTER TABLE `flows` DROP COLUMN `workflow_scenario_id`;
