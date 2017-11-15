9dc86a205ce1dbe73c6bea93eb6b5ec701b12fc8
praetorian <filippov.a@e154.ru>
2017-11-15 21:09:32 +0700

    embed storage

--- api/core/message.go
+++ api/core/message.go
@@ -1,37 +1,25 @@
 package core
 
-func NewMessage() *Message {
-	return &Message{
-		Pull: make(map[string]interface{}),
-	}
+func NewMessage() (m *Message) {
+	m = &Message{}
+	m.pull = make(map[string]interface{})
+	return
 }
-//TODO refactor message system
+
 type Message struct {
-	Error       	string
-	Pull			map[string]interface{}
+	Error		string
+	Storage
 }
-//TODO refactor message system
+
 func (m *Message) clearError() {
 	m.Error = ""
 }
-//TODO refactor message system
+
 func (m *Message) SetError(err string) {
 	m.Error = err
 }
 
-func (m *Message) SetVar(key string, val interface{}) {
-	m.Pull[key] = val
-}
-
-func (m *Message) GetVar(key string) interface{} {
-	if value, ok := m.Pull[key]; ok {
-		return value
-	}
-
-	return nil
-}
-
 func (m *Message) Clear() {
-	m.Pull = make(map[string]interface{})
+	m.pull = make(map[string]interface{})
 	m.Error = ""
 }
