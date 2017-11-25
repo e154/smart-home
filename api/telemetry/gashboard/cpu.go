cb5b8b536b99cf496bb127795a40c3289ac891a8
praetorian <filippov.a@e154.ru>
2017-11-26 00:31:52 +0700

    split telemetry to two module dashboard and map

---
+++ api/telemetry/gashboard/cpu.go
@@ -0,0 +1,55 @@
+package dasboard
+
+import (
+	"github.com/shirou/gopsutil/cpu"
+)
+
+type Processors struct {
+	Processors	[]cpu.InfoStat			`json:"processors"`
+}
+
+func NewCpu() (_cpu *Cpu) {
+
+	cpuinfo, err := cpu.Info()
+	if err != nil || len(cpuinfo) == 0 {
+		return
+	}
+
+	_cpu = &Cpu{
+		Cpuinfo:			&Processors{
+								Processors: cpuinfo,
+							},
+		Usage:				make(map[int]float64),
+		cpu_prev_total:		make(map[int]uint64),
+		cpu_prev_idle:		make(map[int]uint64),
+	}
+
+	return
+}
+
+type Cpu struct {
+	Cpuinfo				*Processors 		`json:"processors"`
+	All					float64				`json:"all"`
+	Usage				map[int]float64		`json:"usage"`
+	cpu_prev_total		map[int]uint64
+	cpu_prev_idle		map[int]uint64
+	all_cpu_prev_total	float64
+	all_cpu_prev_idle	float64
+}
+
+func (m *Cpu) Update() {
+
+	cpuinfo, err := cpu.Info()
+	if err != nil || len(cpuinfo) == 0 {
+		return
+	}
+
+	timeStats, _ := cpu.Times(false)
+	total := timeStats[0].Total()
+	diff_idle := float64(timeStats[0].Idle - m.all_cpu_prev_idle)
+	diff_total := float64(total - m.all_cpu_prev_total)
+	m.All = 100 * (diff_total - diff_idle) / diff_total
+	m.all_cpu_prev_total = total
+	m.all_cpu_prev_idle = timeStats[0].Idle
+
+}
--- api/telemetry/gashboard/uptime.go
+++ api/telemetry/gashboard/uptime.go
@@ -1,29 +1,17 @@
 package dasboard
 
 import (
-	"io/ioutil"
-	"strings"
-	"strconv"
+	"github.com/shirou/gopsutil/host"
 )
 
 type Uptime struct {
-	Total float64 `json:"total"`
-	Idle  float64 `json:"idle"`
+	Total uint64 `json:"total"`
+	Idle  uint64 `json:"idle"`
 }
 
 func (u *Uptime) Update() (*Uptime, error) {
-	b, err := ioutil.ReadFile("/proc/uptime")
-	if err != nil {
-		return nil, err
-	}
 
-	fields := strings.Fields(string(b))
-	if u.Total, err = strconv.ParseFloat(fields[0], 64); err != nil {
-		return nil, err
-	}
-	if u.Idle, err = strconv.ParseFloat(fields[1], 64); err != nil {
-		return nil, err
-	}
+	u.Total, _ = host.Uptime()
 
 	return u, nil
 }
