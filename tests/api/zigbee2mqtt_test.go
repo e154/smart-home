f3e987eead1cb968b245f94ffc329b35d75c27c2
delta54 <support@e154.ru>
2020-02-24 14:44:50 +0700

    issues #107 zigbee2mqtt binding

---
+++ tests/api/zigbee2mqtt_test.go
@@ -0,0 +1,185 @@
+// This file is part of the Smart Home
+// Program complex distribution https://github.com/e154/smart-home
+// Copyright (C) 2016-2020, Filippov Alex
+//
+// This library is free software: you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 3 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Library General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library.  If not, see
+// <https://www.gnu.org/licenses/>.
+
+package api
+
+import (
+	"github.com/e154/smart-home/adaptors"
+	"github.com/e154/smart-home/api/server"
+	"github.com/e154/smart-home/system/access_list"
+	"github.com/e154/smart-home/system/core"
+	"github.com/e154/smart-home/system/migrations"
+	"github.com/e154/smart-home/system/scripts"
+	. "github.com/smartystreets/goconvey/convey"
+	"testing"
+)
+
+//v1.POST("/zigbee2mqtt", s.af.Auth, s.ControllersV1.Zigbee2mqtt.Add)
+//v1.GET("/zigbee2mqtt/:id", s.af.Auth, s.ControllersV1.Zigbee2mqtt.GetById)
+//v1.PUT("/zigbee2mqtt/:id", s.af.Auth, s.ControllersV1.Zigbee2mqtt.Update)
+//v1.DELETE("/zigbee2mqtt/:id", s.af.Auth, s.ControllersV1.Zigbee2mqtt.Delete)
+//v1.GET("/zigbee2mqtts", s.af.Auth, s.ControllersV1.Zigbee2mqtt.GetList)
+//v1.POST("/zigbee2mqtt/:id/reset", s.af.Auth, s.ControllersV1.Zigbee2mqtt.Reset)
+//v1.POST("/zigbee2mqtt/:id/device_ban", s.af.Auth, s.ControllersV1.Zigbee2mqtt.DeviceBan)
+//v1.POST("/zigbee2mqtt/:id/device_whitelist", s.af.Auth, s.ControllersV1.Zigbee2mqtt.DeviceWhitelist)
+//v1.GET("/zigbee2mqtt/:id/networkmap", s.af.Auth, s.ControllersV1.Zigbee2mqtt.Networkmap)
+//v1.POST("/zigbee2mqtt/:id/update_networkmap", s.af.Auth, s.ControllersV1.Zigbee2mqtt.UpdateNetworkmap)
+
+func TestZigbee2mqtt(t *testing.T) {
+
+	Convey("POST /zigbee2mqtt", t, func(ctx C) {
+		err := container.Invoke(func(adaptors *adaptors.Adaptors,
+			migrations *migrations.Migrations,
+			scriptService *scripts.ScriptService,
+			server *server.Server,
+			core *core.Core,
+			accessList *access_list.AccessListService, ) {
+
+
+		})
+		if err != nil {
+			panic(err.Error())
+		}
+	})
+
+	Convey("GET /zigbee2mqtt/{id}", t, func(ctx C) {
+		err := container.Invoke(func(adaptors *adaptors.Adaptors,
+			migrations *migrations.Migrations,
+			scriptService *scripts.ScriptService,
+			server *server.Server,
+			core *core.Core) {
+
+
+		})
+		if err != nil {
+			panic(err.Error())
+		}
+	})
+
+	Convey("PUT /zigbee2mqtt/{id}", t, func(ctx C) {
+		err := container.Invoke(func(adaptors *adaptors.Adaptors,
+			migrations *migrations.Migrations,
+			scriptService *scripts.ScriptService,
+			server *server.Server,
+			core *core.Core) {
+
+
+		})
+		if err != nil {
+			panic(err.Error())
+		}
+	})
+
+	Convey("DELETE /zigbee2mqtt/{id}", t, func(ctx C) {
+		err := container.Invoke(func(adaptors *adaptors.Adaptors,
+			migrations *migrations.Migrations,
+			scriptService *scripts.ScriptService,
+			server *server.Server,
+			core *core.Core) {
+
+
+		})
+		if err != nil {
+			panic(err.Error())
+		}
+	})
+
+	Convey("GET /zigbee2mqtts", t, func(ctx C) {
+		err := container.Invoke(func(adaptors *adaptors.Adaptors,
+			migrations *migrations.Migrations,
+			scriptService *scripts.ScriptService,
+			server *server.Server,
+			core *core.Core) {
+
+
+		})
+		if err != nil {
+			panic(err.Error())
+		}
+	})
+
+	Convey("POST /zigbee2mqtt/{id}/reset", t, func(ctx C) {
+		err := container.Invoke(func(adaptors *adaptors.Adaptors,
+			migrations *migrations.Migrations,
+			scriptService *scripts.ScriptService,
+			server *server.Server,
+			core *core.Core) {
+
+
+		})
+		if err != nil {
+			panic(err.Error())
+		}
+	})
+
+	Convey("POST /zigbee2mqtt/{id}/device_ban", t, func(ctx C) {
+		err := container.Invoke(func(adaptors *adaptors.Adaptors,
+			migrations *migrations.Migrations,
+			scriptService *scripts.ScriptService,
+			server *server.Server,
+			core *core.Core) {
+
+
+		})
+		if err != nil {
+			panic(err.Error())
+		}
+	})
+
+	Convey("POST /zigbee2mqtt/{id}/device_whitelist", t, func(ctx C) {
+		err := container.Invoke(func(adaptors *adaptors.Adaptors,
+			migrations *migrations.Migrations,
+			scriptService *scripts.ScriptService,
+			server *server.Server,
+			core *core.Core) {
+
+
+		})
+		if err != nil {
+			panic(err.Error())
+		}
+	})
+
+	Convey("GET /zigbee2mqtt/{id}/networkmap", t, func(ctx C) {
+		err := container.Invoke(func(adaptors *adaptors.Adaptors,
+			migrations *migrations.Migrations,
+			scriptService *scripts.ScriptService,
+			server *server.Server,
+			core *core.Core) {
+
+
+		})
+		if err != nil {
+			panic(err.Error())
+		}
+	})
+
+	Convey("POST /zigbee2mqtt/{id}/update_networkmap", t, func(ctx C) {
+		err := container.Invoke(func(adaptors *adaptors.Adaptors,
+			migrations *migrations.Migrations,
+			scriptService *scripts.ScriptService,
+			server *server.Server,
+			core *core.Core) {
+
+
+		})
+		if err != nil {
+			panic(err.Error())
+		}
+	})
+}
