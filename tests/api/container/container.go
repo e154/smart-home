2c05e81823c2ed099dc674f70537dcce21fe3d7f
delta54 <support@e154.ru>
2022-01-21 02:11:15 +0700

    github actions

--- tests/api/container/container.go
+++ tests/api/container/container.go
@@ -86,6 +86,7 @@ func BuildContainer() (container *dig.Container) {
 	container.Provide(func() (conf *models.AppConfig, err error) {
 		conf, err = config.ReadConfig("conf", "config.json", "")
 		conf.PgName = "smart_home_test"
+		conf.PgHost = "postgres"
 		conf.Logging = false
 		return
 	})
