ebd147df514c11241b4dc307024e6aac2636b599
Filippov Alex <support@e154.ru>
2023-09-01 11:53:33 +0600

    technical debt

--- static_source/admin/src/views/Dashboard/card_items/button/index.vue
+++ static_source/admin/src/views/Dashboard/card_items/button/index.vue
@@ -11,8 +11,6 @@ const {t} = useI18n()
 // ---------------------------------
 // common
 // ---------------------------------
-const item = ref<CardItem>({} as CardItem)
-
 const props = defineProps({
   item: {
     type: Object as PropType<Nullable<CardItem>>,
@@ -26,18 +24,6 @@ onMounted(() => {
   // store dom element moveable
   props.item.setTarget(el.value)
 })
-
-watch(
-    () => props.item,
-    (val?: CardItem) => {
-      if (!val) return;
-      item.value = val;
-    },
-    {
-      deep: true,
-      immediate: true
-    }
-)
 
 // ---------------------------------
 // component methods
