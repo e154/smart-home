f274a6338a6f64b524d59c5d102eb2f9a3cb855f
Filippov Alex <support@e154.ru>
2023-12-10 17:58:17 +0530

    gate

--- tests/system/container/config.go
+++ tests/system/container/config.go
@@ -27,7 +27,7 @@ import (
 
 func ReadConfig() (conf *models.AppConfig) {
 	conf = &models.AppConfig{}
-	config.ReadConfig(path.Join("config", "config.json"), "", conf)
+	config.ReadConfig(path.Join("conf", "config.json"), "", conf)
 	conf.PgName = "smart_home_test"
 	conf.Logging = false
 	return
