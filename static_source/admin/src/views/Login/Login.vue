189f1716fb3612066c0b7ecd9f5b08058a9564e9
Filippov Alex <support@e154.ru>
2024-01-06 21:14:58 +0530

    technical debt

--- static_source/admin/src/views/Login/Login.vue
+++ static_source/admin/src/views/Login/Login.vue
@@ -34,21 +34,21 @@ const toLogin = () => {
       <div
         :class="`${prefixCls}__left flex-1 bg-gray-500 bg-opacity-20 relative p-30px <xl:hidden`"
       >
-        <div class="flex items-center relative text-white">
-          <img src="../../assets/svgs/logo-w.svg" alt="" class="w-48px h-48px mr-10px" />
-          <span class="text-20px font-bold">{{ underlineToHump(appStore.getTitle) }}</span>
-        </div>
+<!--        <div class="flex items-center relative text-white">-->
+<!--          <img src="../../assets/svgs/logo-w.svg" alt="" class="w-48px h-48px mr-10px" />-->
+<!--          <span class="text-20px font-bold">{{ underlineToHump(appStore.getTitle) }}</span>-->
+<!--        </div>-->
         <div class="flex justify-center items-center h-[calc(100%-60px)]">
           <TransitionGroup
             appear
             tag="div"
             enter-active-class="animate__animated animate__bounceInLeft"
           >
-            <img src="@/assets/svgs/login-box-bg.svg" key="1" alt="" class="w-350px" />
-            <div class="text-3xl text-white" key="2">{{ t('login.welcome') }}</div>
-            <div class="mt-5 font-normal text-white text-14px" key="3">
-              {{ t('login.message') }}
-            </div>
+            <img src="@/assets/svgs/banner-v4.svg" key="1" alt="" class="w-400px" />
+<!--            <div class="text-3xl text-white" key="2">{{ t('login.welcome') }}</div>-->
+<!--            <div class="mt-5 font-normal text-white text-14px" key="3">-->
+<!--              {{ t('login.message') }}-->
+<!--            </div>-->
           </TransitionGroup>
         </div>
       </div>
