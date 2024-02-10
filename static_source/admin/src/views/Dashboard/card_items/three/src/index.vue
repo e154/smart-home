f3f77ae0239d4a5326a1506de8d20375e6552b0b
Filippov Alex <support@e154.ru>
2024-02-10 14:42:41 +0530

    google chrome application

--- static_source/admin/src/views/Dashboard/card_items/three/src/index.vue
+++ static_source/admin/src/views/Dashboard/card_items/three/src/index.vue
@@ -11,14 +11,16 @@ import {
   AmbientLight,
   PointLight,
 } from 'troisjs';
+import Stats from './components/Stats'
 import {Vector3} from 'three';
 import {CardItem} from "@/views/Dashboard/core/core";
-import {RenderVar} from "@/views/Dashboard/core/render";
 import {prepareUrl} from "@/utils/serverId";
+import {Pane} from 'tweakpane';
 
 // ---------------------------------
 // common
 // ---------------------------------
+
 
 const props = defineProps({
   item: {
@@ -28,8 +30,21 @@ const props = defineProps({
 })
 
 const el = ref(null)
+const renderer = ref(null)
 const camera = ref(null)
 const box = ref(null)
+
+const params = ref<Object>({ color: '#ffffff',
+  metalness: 1,
+  roughness: 0.2,
+  light1Color: '#FFFF80',
+  light2Color: '#DE3578',
+  light3Color: '#FF4040',
+  light4Color: '#0d25bb'})
+
+const boxPosition = ref({y: 0, z: 0})
+const planePosition = ref({y: 0, z: 0})
+const cameraPosition = ref({y: 3, z: 2})
 
 onMounted(() => {
   // store dom element moveable
@@ -38,7 +53,13 @@ onMounted(() => {
   // console.log(box.value.mesh.position)
   camera.value.camera.lookAt(box.value.mesh.position)
 
-  // console.log(camera.value.camera)
+  const pane = new Pane({
+    container: el.value,
+  });
+  pane.addBinding(params.value, 'color');
+  pane.addBinding(params.value, 'metalness', { min: 0, max: 1 });
+  pane.addBinding(params.value, 'roughness', { min: 0, max: 1 });
+
 })
 
 // ---------------------------------
@@ -55,27 +76,27 @@ const boxClick = (e) => {
   console.log(e);
 }
 
-const boxPosition = ref({y: 3, z: 0})
-const planePosition = ref({y: 0, z: 0})
-const cameraPosition = ref({y: 3, z: 2})
+
 
 const getUrl = (url: string): string => {
   return prepareUrl(import.meta.env.VITE_API_BASEPATH as string + url);
 }
 
-
 </script>
 
 <template>
-  <div ref="el">
+  <div ref="el" id="three-container">
 
     <Renderer ref="renderer" antialias resize
               :orbit-ctrl="{ autoRotate: true, enableDamping: true, dampingFactor: 0.05 }">
+
       <Camera ref="camera" :position="cameraPosition"/>
+      <Stats :no-setup="false">
+
       <Scene>
         <AmbientLight :position="{ y: 50, z: 50 }"/>
         <PointLight :position="{ y: 50, z: 50 }"/>
-        <Plane ref="plane" :position="planePosition"/>
+<!--        <Plane ref="plane" :position="planePosition"/>-->
         <Box ref="box" @pointerOver="boxOver" @click="boxClick" :position="boxPosition"
              :rotation="{ y: Math.PI / 4, z: Math.PI / 4 }">
           <PhongMaterial>
@@ -83,10 +104,16 @@ const getUrl = (url: string): string => {
           </PhongMaterial>
         </Box>
       </Scene>
+      </Stats>
+
     </Renderer>
   </div>
 </template>
 
 <style lang="less">
-
+.tp-rotv {
+  position: absolute;
+  top: 0;
+  right: 0;
+}
 </style>
