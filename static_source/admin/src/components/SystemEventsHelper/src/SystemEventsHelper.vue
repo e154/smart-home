b71098945d716b428671026ce92bdad4486e2d5b
Filippov Alex <support@e154.ru>
2024-06-23 17:47:53 +0700

    added help notice for system event trigger

--- static_source/admin/src/components/SystemEventsHelper/src/SystemEventsHelper.vue
+++ static_source/admin/src/components/SystemEventsHelper/src/SystemEventsHelper.vue
@@ -3,8 +3,9 @@
 
 <template>
   <div class="tip custom-block w-[100%]">
-    <p>System events list</p>
+    <p><strong>System events list</strong></p>
 
+    <div><strong>tasks</strong></div>
     event_task_completed
     event_created_task_model
     event_removed_task_model
@@ -30,11 +31,13 @@
     event_removed_condition_model
     event_updated_condition_model
 
+    <div><strong>backup</strong></div>
     event_created_backup
     event_removed_backup
     event_uploaded_backup
     event_started_restore
 
+    <div><strong>entity</strong></div>
     event_state_changed
     event_last_state_changed
     event_get_last_state
@@ -48,29 +51,37 @@
     event_entity_loaded
     event_entity_set_state
 
+    <div><strong>mqtt</strong></div>
     event_mqtt_new_client
 
+    <div><strong>plugins</strong></div>
     event_plugin_loaded
     event_plugin_unloaded
 
+    <div><strong>scripts</strong></div>
     event_created_script_model
     event_updated_script_model
     event_removed_script_model
 
+    <div><strong>services</strong></div>
     event_service_started
     event_service_stopped
     event_service_restarted
 
+    <div><strong>users</strong></div>
     event_update_user_location
     event_direct_message
     event_user_signed_in
 
+    <div><strong>variables</strong></div>
     event_updated_variable_model
     event_removed_variable_model
 
-    eventServerVersion
-    eventGetServerVersion
+    <div><strong>server version</strong></div>
+    event_server_version
+    event_get_server_version
 
+    <div><strong>zigbee2mqtt</strong></div>
     event_created_zigbee2mqtt_model
     event_updated_zigbee2mqtt_model
     event_removed_zigbee2mqtt_model
